ce8977b4590cb648b550a0500b68b00883e4c074
64dude <Michael.Wagner@emc.com>
2015-11-13 13:54:49 -0800

    Added Rectangle subclass

--- src/Shapes.java
+++ src/Shapes.java
@@ -22,6 +22,13 @@ class Triangle extends TwoDShape {
 	// System.out.println("Area: " + area(w, h));
 }
 
+class Rectangle extends TwoDShape {
+	
+	double area(double w, double h) {
+		return w * h;
+	}
+}
+
 
 public class Shapes {
 
@@ -32,15 +39,20 @@ public class Shapes {
 		// TODO Auto-generated method stub
 		Triangle t1 = new Triangle();
 		Triangle t2 = new Triangle();
+		Rectangle r1 = new Rectangle();
 		t1.h = 10;
 		t1.w = 20;
 		t2.h = 5;
 		t2.w = 3;
+		r1.h = 6;
+		r1.w = 3;
 		
 		t1.printDim();
 		t2.printDim();
+		r1.printDim();
 		System.out.println("T1 area: " + t1.area(t1.h, t1.w));
 		System.out.println("T2 area: " + t1.area(t2.h, t2.w));
+		System.out.println("R1 area: " + r1.area(r1.w, r1.h));
 
 	}
 
